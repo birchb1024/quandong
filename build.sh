6bd392799134205a8978b926700d56910f9d8ea6
Bill Birch <bill.birch@coles.com.au>
2021-06-15 16:09:27 +1000

    Starting to work

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@ set -euo pipefail
 scriptdir="$(readlink -f "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )")"
 : "$scriptdir"
 program_name=quandong
+
+mkdir -p package bin
+
 git describe
 
 export CGO_ENABLED=0 # For static build without 'C'
@@ -12,9 +15,9 @@ function linux_compile {
   export GOOS=linux
   export GOARCH="$1"
   echo "$GOOS" "$GOARCH"
-  rm -f "${program_name}""$GOOS"_"$GOARCH".tgz
-  go build -a -ldflags="-X 'main.Version=$(git describe)'" -o "${program_name}""$GOOS"_"$GOARCH" frangipanni.go
-  tar zcf "${program_name}""$GOOS"_"$GOARCH".tgz "${program_name}""$GOOS"_"$GOARCH" ./*.lua
+  rm -f package/"${program_name}""$GOOS"_"$GOARCH".tgz
+  go build -a -ldflags="-X 'main.Version=$(git describe)'" -o bin/"${program_name}"_"$GOOS"_"$GOARCH" "${program_name}".go
+  tar zcf package/"${program_name}"_"$GOOS"_"$GOARCH".tgz bin/"${program_name}"_"$GOOS"_"$GOARCH"
 }
 
 function cross_compile {
@@ -23,12 +26,15 @@ function cross_compile {
   export GOARCH="${1#*/}"         # This is why bash is so awful
   echo "$GOOS" "$GOARCH"
   rm -f "${program_name}""$GOOS"_"$GOARCH".zip
-  go build -a -ldflags="-X 'main.Version=$(git describe)'" -o "${program_name}""$GOOS"_"$GOARCH" frangipanni.go
-  zip --quiet "${program_name}""$GOOS"_"$GOARCH".zip "${program_name}""$GOOS"_"$GOARCH" ./*.lua
+  go build -a -ldflags="-X 'main.Version=$(git describe)'" -o bin/"${program_name}"_"$GOOS"_"$GOARCH" "${program_name}".go
+  zip --quiet package/"${program_name}"_"$GOOS"_"$GOARCH".zip bin/"${program_name}"_"$GOOS"_"$GOARCH"
 }
 
-test/confidence.sh
-
+linux_compile amd64
+cd bin
+ln -sf quandong_linux_amd64 "$PWD"/quandong
+ln -sf quandong_linux_amd64 "$PWD"/ls
+exit 0
 for arch in 386 arm64 amd64
 do
   linux_compile "$arch"
--- quandong.go
+++ quandong.go
@@ -1,18 +1,64 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
+	"strings"
+	"syscall"
 )
 
 var Version = "none"
-var printVersion bool
 
+// findProgramInPath - Look for the the real program we are usurping,
+// return the path to it as a string.
+func findProgramInPath(name string) (string, error) {
+	PATH, ok := os.LookupEnv("PATH")
+	if !ok {
+		log.Fatal("ERROR: No PATH environment variable")
+	}
+	pathList := strings.Split(PATH, ":")
+	for _, dir := range pathList {
+		files, err := ioutil.ReadDir(dir)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, f := range files {
+			if f.Name() == name && !f.Mode().IsDir() && f.Mode().IsRegular() && (f.Mode()&0111) != 0 {
+				return fmt.Sprintf("%s/%s", dir, f.Name()), nil
+			}
+		}
+	}
+	return "", fmt.Errorf("ERROR: executable '%s' not found in PATH", name)
+}
 func main() {
 
-	if printVersion {
-		fmt.Println(Version)
-		os.Exit(0)
+	fmt.Printf("%#v\n", os.Args)
+	a0 := strings.Split(os.Args[0], "/")
+	name := a0[len(a0)-1]
+	target, err := findProgramInPath(name)
+	if err != nil {
+		log.Fatal(err)
 	}
 
+	tempPrefix := fmt.Sprintf("quandong-%s-", name)
+	logFile, err := ioutil.TempFile(".", tempPrefix)
+	if err != nil {
+		log.Fatalf("ERROR: Cannot create temporary file: %s. %s", tempPrefix, err)
+	}
+	defer func() { _ = logFile.Close() }()
+
+	var toLog = map[string]interface{}{"Args": os.Args, "Environ": os.Environ(), "Target": target, "quandong": map[string]string{"Version": Version}}
+	js, err := json.MarshalIndent(toLog, "", "  ")
+	n, err := fmt.Fprintln(logFile, string(js))
+	if n == 0 || err != nil {
+		log.Fatal(err)
+	}
+	err = syscall.Exec(target, os.Args, os.Environ())
+	if err != nil {
+		log.Fatal(err)
+	}
 }
